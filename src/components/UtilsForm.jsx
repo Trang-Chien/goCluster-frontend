2c8a0c7fe296e87b243f9992eae9fae0f371d61e
BuiHongChien <chienbui211@gmail.com>
2022-04-26 11:26:41 +0300

    update WelcomePage with animation and background

--- src/components/UtilsForm.jsx
+++ src/components/UtilsForm.jsx
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 
 import { IoIosClose } from "react-icons/io";
+import { DropdownList } from "./Dropdown";
+
+const members = ["member 1", "member 2", "member 3", "member 4", "member 5"];
 
 export const CreateRole = ({ changeOpenCreateRoleForm }) => {
   return (
@@ -17,7 +20,21 @@ export const CreateRole = ({ changeOpenCreateRoleForm }) => {
         </button>
       </div>
 
-      <div className="template__body"></div>
+      <div className="template__body">
+        <input
+          className="input input--template"
+          placeholder="your server name..."
+        />
+
+        <DropdownList barTitle="member list" options={members} />
+
+        <button
+          className="button button--text button--copy"
+          onClick={() => changeOpenCreateRoleForm(false)}
+        >
+          create role
+        </button>
+      </div>
     </div>
   );
 };
@@ -98,18 +115,177 @@ export const InvitePeople = ({ changeOpenInvitePeopleForm }) => {
 };
 
 export const AlertLeaveServer = ({ changeOpenAlertLeaveServer }) => {
-    return (
-      <div className="template__wrapper">
-          <button
-            className="button button--icon button--close"
+  return (
+    <div className="template__wrapper">
+      <button
+        className="button button--icon button--close"
+        onClick={() => {
+          changeOpenAlertLeaveServer(false);
+        }}
+      >
+        <IoIosClose className="icon icon--close" />
+      </button>
+
+      <div className="template__body"></div>
+    </div>
+  );
+};
+
+export const CreateServer = ({ changeOpenCreateServerForm }) => {
+  return (
+    <div className="template__wrapper">
+      <div className="template__header">
+        <div className="template__title">create a new server</div>
+        <button
+          className="button button--icon button--close"
+          onClick={() => {
+            changeOpenCreateServerForm(false);
+          }}
+        >
+          <IoIosClose className="icon icon--close" />
+        </button>
+      </div>
+
+      <div className="template__body">
+        <input
+          className="input input--template"
+          placeholder="your server name..."
+        />
+        <button
+          className="button button--text button--copy"
+          onClick={() => changeOpenCreateServerForm(false)}
+        >
+          create server
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export const CreateChannel = ({ changeOpenCreateChannelForm }) => {
+  const [typeChannel, setTypeChannel] = useState("public");
+  const [showMemberList, setShowMemberList] = useState(false);
+
+  return (
+    <div className="template__wrapper">
+      <div className="template__header">
+        <div className="template__title">create a new channel</div>
+        <button
+          className="button button--icon button--close"
+          onClick={() => {
+            changeOpenCreateChannelForm(false);
+          }}
+        >
+          <IoIosClose className="icon icon--close" />
+        </button>
+      </div>
+
+      <div className="template__body">
+        <input
+          className="input input--template"
+          placeholder="channel name..."
+        />
+        <div className="radio__wrapper">
+          <div
+            className="radio__option"
             onClick={() => {
-              changeOpenAlertLeaveServer(false);
+              setTypeChannel("public");
+              setShowMemberList(false);
             }}
           >
-            <IoIosClose className="icon icon--close" />
-          </button>
-  
-        <div className="template__body"></div>
+            <div
+              className={`radio__dot ${
+                typeChannel === "public"
+                  ? "radio__dot--selected"
+                  : "radio__dot--noselected"
+              }`}
+            ></div>
+            <div className="radio__content">public</div>
+          </div>
+          <div
+            className="radio__option"
+            onClick={() => {
+              setTypeChannel("private");
+              setShowMemberList(true);
+            }}
+          >
+            <div
+              className={`radio__dot ${
+                typeChannel === "private"
+                  ? "radio__dot--selected"
+                  : "radio__dot--noselected"
+              }`}
+            ></div>
+            <div className="radio__content">private</div>
+          </div>
+        </div>
+        {showMemberList ? <div className="listmember"></div> : null}
+
+        <button
+          className="button button--text button--copy"
+          onClick={() => changeOpenCreateChannelForm(false)}
+        >
+          create channel
+        </button>
       </div>
-    );
+    </div>
+  );
+};
+
+export const BackGroundWelcome = () => {
+  function getRandom(min, max) {
+    return Math.random() * (max - min) + min;
+  }
+
+  const renderedStar = () => {
+    const number = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+    return number.map((n) => (
+      <React.Fragment key={n}>
+        <div
+          className="star star--medium"
+          style={{
+            position: "absolute",
+            top: `${getRandom(1,98)}%`,
+            left: `${getRandom(1,98)}%`,
+            borderRadius: `${getRandom(20,70)}% ${getRandom(20,70)}% ${getRandom(
+              20,70
+            )}% ${getRandom(20,70)}% / ${getRandom(20,70)}% ${getRandom(
+              20,70
+            )}% ${getRandom(20,70)}% ${getRandom(20,70)}% `,
+          }}
+        ></div>
+        <div
+          className="star star--large"
+          style={{
+            position: "absolute",
+            top: `${getRandom(2,98)}%`,
+            left: `${getRandom(2,98)}%`,
+            borderRadius: `${getRandom(20,70)}% ${getRandom(20,70)}% ${getRandom(
+              20,70
+            )}% ${getRandom(20,70)}% / ${getRandom(20,70)}% ${getRandom(
+              20,70
+            )}% ${getRandom(20,70)}% ${getRandom(20,70)}% `,
+          }}
+        ></div>
+        <div
+          className="star star--small"
+          style={{
+            position: "absolute",
+            top: `${getRandom(2,98)}%`,
+            left: `${getRandom(2,98)}%`,
+            borderRadius: `${getRandom(20,70)}% ${getRandom(20,70)}% ${getRandom(
+              20,70
+            )}% ${getRandom(20,70)}% / ${getRandom(20,70)}% ${getRandom(
+              20,70
+            )}% ${getRandom(20,70)}% ${getRandom(20,70)}% `,
+          }}
+        ></div>
+      </React.Fragment>
+    ));
   };
+  return (
+    <div className="star__wrapper">
+      <div className="star__container">{renderedStar()}</div>
+    </div>
+  );
+};
--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -4,6 +4,8 @@ import { useNavigate } from "react-router-dom";
 import WelcomeForm from "../components/WelcomeForm";
 import LoginForm from "../components/LoginForm";
 import RegisterForm from "../components/RegisterForm";
+import TextArea from "../components/TextArea";
+import { BackGroundWelcome } from "../components/UtilsForm";
 
 const WelcomePage = (props) => {
   const [isLoggined, setIsLoggined] = useState(false);
@@ -11,10 +13,13 @@ const WelcomePage = (props) => {
   const [showRegisterForm, setShowRegisterForm] = useState(false);
   const [showWelcomeForm, setShowWelcomeForm] = useState(true);
   const navigate = useNavigate();
+  const [showForms, setShowForms] = useState(false);
 
   useEffect(() => {
     if (isLoggined) navigate("/");
     if (!isLoggined) setShowWelcomeForm(true);
+    const timer = setTimeout(() => setShowForms(true), 6500);
+    return () => clearTimeout(timer);
   }, [isLoggined]);
 
   const onChosing = (clickedButton) => {
@@ -44,28 +49,49 @@ const WelcomePage = (props) => {
     setIsLoggined(true);
   };
 
-  const onBack=(event)=>{
+  const onBack = (event) => {
     event.preventDefault();
 
     setShowLoginForm(false);
     setShowRegisterForm(false);
     setShowWelcomeForm(true);
     setIsLoggined(false);
-  }
+  };
 
-  const setData=(firstName, lastName, username)=>{
-    console.log('set data works')
-    localStorage.setItem('firstName', JSON.stringify(firstName))
-    localStorage.setItem('lastName', JSON.stringify(lastName))
-    localStorage.setItem('username', JSON.stringify(username))
-    localStorage.setItem('avatar', JSON.stringify(`${process.env.API_AVATAR}${username}`))
-  }
+  const setData = (firstName, lastName, username) => {
+    console.log("set data works");
+    localStorage.setItem("firstName", JSON.stringify(firstName));
+    localStorage.setItem("lastName", JSON.stringify(lastName));
+    localStorage.setItem("username", JSON.stringify(username));
+    localStorage.setItem(
+      "avatar",
+      JSON.stringify(`${process.env.API_AVATAR}${username}`)
+    );
+  };
 
   return (
     <div className="bg">
-      {showWelcomeForm ? <WelcomeForm choice={onChosing} /> : null}
-      {showLoginForm ? <LoginForm logined={onLogin} back={onBack} /> : null}
-      {showRegisterForm ? <RegisterForm registered={onRegister} back={onBack} setDate={setData} /> : null}
+      {/* <div className="bg__message"> */}
+      {/* <div className="bg__content">start goCluster</div>
+        <div className="bg__status bg__status-1">seding...</div>
+        <div className="bg__status bg__status-2">sent</div>
+        <div className="bg__status bg__status-3">seen</div> */}
+      {/* </div> */}
+      <TextArea page="welcome" />
+      {showForms ? (
+        <React.Fragment>
+          <BackGroundWelcome />
+          {showWelcomeForm ? <WelcomeForm choice={onChosing} /> : null}
+          {showLoginForm ? <LoginForm logined={onLogin} back={onBack} /> : null}
+          {showRegisterForm ? (
+            <RegisterForm
+              registered={onRegister}
+              back={onBack}
+              setDate={setData}
+            />
+          ) : null}
+        </React.Fragment>
+      ) : null}
     </div>
   );
 };
