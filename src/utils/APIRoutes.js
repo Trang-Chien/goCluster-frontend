1f8b41683e88602b2f28150d61035749511f4c85
BuiHongChien <chienbui211@gmail.com>
2022-05-03 12:43:56 +0300

    add APIRoutes

---
+++ src/utils/APIRoutes.js
@@ -0,0 +1,9 @@
+export const host = '';
+export const loginRoute = `${host}login`;
+export const registerRoute = `${host}register`;
+export const logoutRoute = `${host}/logout`;
+export const allConvesationRoute = `${host}/allconvesation`;
+export const sendMessageRoute = `${host}/messages/addmsg`;
+export const recieveMessageRoute = `${host}/messages/getmsg`;
+
+export const avatarAPI ='https://api.multiavatar.com';
