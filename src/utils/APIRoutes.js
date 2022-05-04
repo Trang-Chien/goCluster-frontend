fbbb9fb1d89b40472bf9dd1c1d13088dd4c86786
BuiHongChien <chienbui211@gmail.com>
2022-05-04 13:45:59 +0300

    update API routes

--- src/utils/APIRoutes.js
+++ src/utils/APIRoutes.js
@@ -1,8 +1,17 @@
 export const host = '';
-export const loginRoute = `${host}login`;
-export const registerRoute = `${host}register`;
+
+export const loginRoute = `${host}/login`;
+export const registerRoute = `${host}/register`;
 export const logoutRoute = `${host}/logout`;
-export const allConvesationRoute = `${host}/allconvesation`;
+
+export const createServerRoute = `${host}/newworkspace`;
+export const createChannelRoute = `${host}/newchannel`;
+export const createDirectMessageRoute = `${host}/newdirectmessage`;
+
+
+export const allDirectMessageRoute = `${host}/alldirectmessage`;
+export const allServerRoute = `${host}/allworkspace`;
+
 export const sendMessageRoute = `${host}/messages/addmsg`;
 export const recieveMessageRoute = `${host}/messages/getmsg`;
 
